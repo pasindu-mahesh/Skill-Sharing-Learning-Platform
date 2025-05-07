d55f6fe6c5955a3afeb92260495510a7ce57604e
Manashaa <sewmini73karunarathne@gmail.com>
2025-05-07 16:59:04 +0530

    backend config

---
+++ server/src/main/java/com/server/server/config/MongoConfig.java
@@ -0,0 +1,10 @@
+package com.photoprogress.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+    // Additional MongoDB configuration if needed
+}
