f5ee40ae049800f0569050dae7950c2b30057ac7
Senarathna.K.H <kaveeshaheshan590@gmail.com>
2025-05-07 17:50:43 +0530

    backend update

--- server/src/main/java/com/server/server/SkillsharingApplication.java
+++ server/src/main/java/com/server/server/SkillsharingApplication.java
@@ -1,13 +1,13 @@
-package com.server.server;
+package com.paf.skillsharing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerApplication {
+public class SkillsharingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
+		SpringApplication.run(SkillsharingApplication.class, args);
 	}
 
 }
